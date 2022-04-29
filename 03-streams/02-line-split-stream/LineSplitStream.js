978d90fdb8ffdb1ddfe9458120462529adb1ad32
ymatuhin <ym@ymatuhin.ru>
2022-04-29 16:55:25 +0300

    03-02 line split stream

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -1,15 +1,25 @@
-const stream = require('stream');
-const os = require('os');
+const os = require("os");
+const { Transform } = require("stream");
 
-class LineSplitStream extends stream.Transform {
+class LineSplitStream extends Transform {
   constructor(options) {
     super(options);
+    this.leftOver = "";
   }
 
-  _transform(chunk, encoding, callback) {
+  _transform(chunk, _, callback) {
+    const str = this.leftOver + chunk.toString("utf-8");
+    const splitted = str.split(os.EOL);
+    this.leftOver = splitted.pop();
+    splitted.forEach((line) => {
+      this.push(line, "utf-8");
+    });
+    callback();
   }
 
   _flush(callback) {
+    this.push(this.leftOver, "utf-8");
+    callback();
   }
 }
 
